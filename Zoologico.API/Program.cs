bb58ea66734be16dfa1b715b9b1d2a859c729274
ANDY JOEL GUAMBA ROMAN <ajguambar@utn.edu.ec>
2025-12-02 22:45:15 -0500

    Cambios1

--- Zoologico.API/Program.cs
+++ Zoologico.API/Program.cs
@@ -21,10 +21,8 @@ namespace Zoologico.API
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            
                 app.UseSwagger();
                 app.UseSwaggerUI(); 
-
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
